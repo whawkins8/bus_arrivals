dd2e8a73a527c9e8a53fee9294fe8db64256eb83
William Hawkins <whawkins888@gmail.com>
2019-12-13 09:01:14 -0500

    finished GetBuses, now pulls the data for bus location and arrival stops, and stores in a Bus object for processing.

--- GetBuses.py
+++ GetBuses.py
@@ -1,9 +1,11 @@
 import requests
+from RouteAndStop import RouteAndStop
 from config import get_transloc_key
 
 class Bus:
-    def __init__(self, ID, arrivals):
+    def __init__(self, ID, loc, arrivals):
         self.ID = ID
+        self.loc = loc
         self.arrivals = arrivals
 
 class GetBuses:
@@ -17,12 +19,12 @@ class GetBuses:
         self.headers = {'x-rapidapi-host': "transloc-api-1-2.p.rapidapi.com",
                         'x-rapidapi-key': get_transloc_key()}
 
-        self.get_buses()
+        self.get_response()
 
     def get_response(self):
         try:
-            response = requests.request("GET", url, headers=self.headers, params=self.querystring)
-            response.raise_for_status()
+            self.response = requests.request("GET", self.url, headers=self.headers, params=self.querystring)
+            self.response.raise_for_status()
 
         except requests.HTTPError as http_err:
             print(f'HTTP error occurred: {http_err}')
@@ -32,12 +34,20 @@ class GetBuses:
             self.parse_response()
 
     def parse_response(self):
-        data = self.response.json()['data']["20"]
-
+        data = self.response.json()['data'][self.querystring['agencies']]
+        
         for bus in data:
-            ID = data['vehicle_id']
-            loc = data['location']
-            
+            ID = bus['vehicle_id']
+            loc = bus['location']
+            arriv = bus['arrival_estimates']
+            newbus = Bus(ID, loc, arriv)
+            self.buses.append(newbus)
+    
+        
+        
+if __name__ == '__main__':
+    routeinfo = RouteAndStop("20", "4000034", "4101146")
+    b = GetBuses(routeinfo)
 
 
 
